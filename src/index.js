2a8338e8cd41ed3e28d1f3b5196590ac2d1d52e0
2rchinsky <denisturukin@mail.ru>
2022-05-22 16:40:51 +0400

    Add new blocks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./scss/main.scss";
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+});

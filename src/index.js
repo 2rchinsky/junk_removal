683becc077bb6fa73b4864fa8efba8159757fafd
2rchinsky <denisturukin@mail.ru>
2022-06-15 02:52:01 +0400

    for brat

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ $('.block').html('jQuery is working');
 
 console.log(user)
 new Glider(document.querySelector('.glider'), {
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     draggable: true,
     dots: '.dots',
